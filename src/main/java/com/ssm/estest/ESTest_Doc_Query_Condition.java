25ff54f6018462fd712831360c2631d9530fda68
shaoshao <1600767556@qq.com>
2022-11-26 13:07:08 +0800

    文档 条件查询数据

---
+++ src/main/java/com/ssm/estest/ESTest_Doc_Query_Condition.java
@@ -0,0 +1,41 @@
+package com.ssm.estest;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+
+/**
+ * @author shaoshao
+ * @Date 2022/11/25 19:26
+ * @Description: 文档 条件查询数据
+ */
+public class ESTest_Doc_Query_Condition {
+    public static void main(String[] args) throws Exception {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+        // 条件查询索引中数据 termQuery
+        SearchRequest request = new SearchRequest();
+        request.indices("user");
+        // 查询年龄为20的
+//        request.source(new SearchSourceBuilder().query(QueryBuilders.termQuery("age",20)));
+        request.source(new SearchSourceBuilder().query(QueryBuilders.termsQuery("age","20","22")));
+
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        SearchHits hits = response.getHits();
+        System.out.println(hits.getTotalHits());
+        System.out.println(response.getTook());
+        for (SearchHit hit : hits) {
+            System.out.println(hit.getSourceAsString());
+        }
+        
+        client.close();
+    }
+}
