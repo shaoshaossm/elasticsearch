f2bad77afb94f1dad54d294dd6e24591075c84e6
shaoshao <1600767556@qq.com>
2022-11-26 13:49:36 +0800

    最大值查询

---
+++ src/main/java/com/ssm/estest/ESTest_Doc_Query_Max.java
@@ -0,0 +1,45 @@
+package com.ssm.estest;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+
+/**
+ * @author shaoshao
+ * @Date 2022/11/25 19:26
+ * @Description: 最大值查询数据
+ */
+public class ESTest_Doc_Query_Max {
+    public static void main(String[] args) throws Exception {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+        SearchRequest request = new SearchRequest();
+        request.indices("user");
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        AggregationBuilder aggregationBuilder = AggregationBuilders.max("maxAge").field("age");
+
+        builder.aggregation(aggregationBuilder);
+        request.source(builder);
+
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+
+        SearchHits hits = response.getHits();
+        System.out.println(hits.getTotalHits());
+        System.out.println(response.getTook());
+        for (SearchHit hit : hits) {
+            System.out.println(hit.getSourceAsString());
+        }
+
+        client.close();
+    }
+}
